9fe820f878bca00318571874ff0425dee8bcc918
prabinkarki777 <prabin_karki777@hotmail.com>
2020-09-02 14:10:09 +0545

    custom js and black template

---
+++ app/design/frontend/Magento/luma/web/js/custom-menu.js
@@ -0,0 +1,30 @@
+// // (function() {
+// //     var visited = localStorage.getItem('visited');
+// //     if (!visited) {
+// //         document.getElementById("popupp").style.visibility = "visible";
+// //         localStorage.setItem('visited', true);
+// //     }
+// // })();
+
+// console.log("hello take20");
+// require([
+//     "jquery",
+//     "jquery/ui"
+// ],
+// function ($) {
+//     'use strict';
+
+    
+//         // var visited = localStorage.getItem('visited');
+//         // if(visited){
+//         //     console.log("visited");
+//         //     $('.visit').css('display','none');
+//         //    $('.visit').attr('id','newId');
+//         // }
+//         // else{
+//         //     console.log("first time");
+//         //     $(".visit").css("display","flex");
+//         //     localStorage.setItem('visited', true);
+//         // }
+    
+// });
