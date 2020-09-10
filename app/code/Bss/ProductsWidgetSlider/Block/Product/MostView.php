779cddc49c2f208a935b829d239ede3b3c803d10
prabinkarki777 <prabin_karki777@hotmail.com>
2020-09-10 22:17:31 +0545

    fix  merge

--- app/code/Bss/ProductsWidgetSlider/Block/Product/MostView.php
+++ app/code/Bss/ProductsWidgetSlider/Block/Product/MostView.php
@@ -62,6 +62,7 @@ class MostView extends \Bss\ProductsWidgetSlider\Block\GetData
         \Magento\Catalog\Helper\Output $Output,
         \Magento\Framework\Data\Helper\PostHelper $postHelper,
         \Magento\Framework\App\ProductMetadataInterface $productMetadata,
+        \Magento\Catalog\Api\CategoryRepositoryInterface $categoryRepository,
         GetMostViewCollection $bssGetMostViewCollection,
         priceCurrencyInterface $priceCurrencyInterface,
         array $data = []
@@ -80,6 +81,7 @@ class MostView extends \Bss\ProductsWidgetSlider\Block\GetData
             $Output,
             $postHelper,
             $productMetadata,
+            $categoryRepository,
             $priceCurrencyInterface,
             $data
         );
